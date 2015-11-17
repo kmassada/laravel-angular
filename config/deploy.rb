21ce1a01ca6f49767a4ac129657bf9892d6e509f
Kenneth Massada <kmassada@silverstrike.local>
2015-11-16 21:13:20 -0500

    adding CAP's bits

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'laravel-angular'
+set :application, 'pathfinder.tadbit.cc'
 set :repo_url, 'git@github.com:kmassada/laravel-angular.git'
-
-set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/pathfinder.tadbit.cc'
 
 # Default value for :scm is :git
 set :scm, :git
@@ -22,7 +20,7 @@ set :format, :pretty
 set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
@@ -34,9 +32,65 @@ set :log_level, :debug
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
 
 namespace :deploy do
+
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        execute "sudo httpd -k restart"
+      end
+    end
+
+  desc 'Composer - install'
+  task :composer_install do
+    on roles(:app), in: :sequence do
+      within release_path do
+        execute 'cd #{current_path}'
+        execute 'composer install  --no-dev --quiet'
+      end
+    end
+  end
+
+  desc 'Database - setup'
+  task :databas_setup do
+    on roles(:app), in: :sequence do
+      within release_path do
+        upload(".env.production", "#{current_path}/.env")
+      end
+    end
+  end
+
+  desc 'Database - Migration'
+  task :database_migration do
+    on roles(:app), in: :sequence do
+      within release_path do
+        execute 'cd #{current_path}'
+        execute 'yes|php artisan migrate'
+      end
+    end
+  end
+
+  desc 'Database - Seed'
+  task :database_seed do
+    on roles(:app), in: :sequence do
+      within release_path do
+        execute 'cd #{current_path}'
+        execute 'yes|php artisan db:seed'
+      end
+    end
+  end
+
+  desc 'Application - FIxes'
+  task :database_seed do
+    on roles(:app), in: :sequence do
+      within release_path do
+        execute 'cd #{current_path}'
+        execute 'chmod 777 -R storage'
+      end
+    end
+  end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -47,4 +101,7 @@ namespace :deploy do
     end
   end
 
+  before :updated, 'deploy:ghost_install'
+  before :updated, 'deploy:ghost_symlink'
+  after :publishing, :restart
 end
